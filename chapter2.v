7b627984297cc2008ce44a7f92fd1b56a5261fee
Miroslav Dobsicek <m.dobsicek@gmail.com>
2015-06-03 22:48:47 +0200

    Declarations and definitions

--- chapter2.v
+++ chapter2.v
@@ -121,3 +121,119 @@ Check (3 * 4).      (* 3 * 4 : Z *)
      - the formal parametr v:A plays a role of a local variable, whose scope is
        restricted inside the abstraction body
 *)
+
+(* nested abstraction *)
+Check (fun n : nat => fun p : nat => fun z : Z => (Z_of_nat (n + p) + z)%Z).
+Check (fun n p : nat => fun z : Z => (Z_of_nat (n + p) + z)%Z).
+Check (fun (n p :nat) (z : Z) => (Z_of_nat (n + p) +z)%Z).
+
+(*
+  - if the type of a formal parameter can be determined from the body,
+    it can be ommited
+  - unused formal parameters are automatically replaced by anonymous variable _
+*)
+
+Check (fun a b : nat => a).  (* fun a _ : nat => a  : nat -> nat -> nat *)
+
+(* local bindings *)
+(* let v := t1 in t2 *)
+Check (fun n p : nat =>
+         (let diff := n - p in
+          let square := diff * diff in
+            square + p))%nat.
+
+(* renaming of bound variables in abstractions and let-in bindings is
+   a congruence called \alpha conversion *)
+
+(******************************************************************)
+(** 2.3 Declarations and definitions **)
+(* Global declaration, Parameter v:A *)
+Parameter  max_int : Z.   (* max_int is assumed *)
+
+(* Global definition, Definition v:A := t
+   - the type can be inferred
+*)
+Definition min_int := 1 - max_int.   (* min_int is defined *)
+Print min_int.                       (* min_int = 1 - max_int : Z *)
+
+(* Definition of functions - several syntactic form *)
+Definition cube1 := fun z:Z => z*z*z.     (* explicit abstraction *)
+Definition cube2 (z : Z) : Z :=  z*z*z.   (* passing type of input and output *)
+Definition cube3 z :=  z*z*z.             (* just a name of parameter and body *)
+Print cube3.
+(*
+  cube3 = fun z : Z => z * z * z
+     : Z -> Z
+
+  Argument scope is [Z_scope]       (* this is the last opened scope *)
+*)
+
+
+
+Open Scope nat_scope.
+Check cube3.  (* cube3 : Z -> Z, the scope of/from the definition is remembered *)
+
+(* Sections
+   controlling scope of local variables
+
+   local declaration: Variable a : A
+   local definition:  Let x := t
+*)
+
+Open Scope Z_scope.
+
+Section binomial_def.
+    Variables a b : Z.
+    Definition binomial z : Z := a*z + b.
+    Print binomial.
+    (*
+       binomial = fun z : Z => a * z + b
+         : Z -> Z
+
+       a, b are in the context
+    *)
+    Section trinomial_def.
+        Variable c : Z.
+        Definition trinomial z : Z := (binomial z)*z + c.
+    End trinomial_def.
+End binomial_def.
+
+(* all used local variables get added as an abstraction
+   after section is closed - the variables are not any more
+   available in the context and need to be added as formal
+   parameters
+*)
+Print binomial.
+(*
+    binomial = fun a b z : Z => a * z + b
+        : Z -> Z -> Z -> Z
+*)
+
+Print trinomial.
+(*
+    trinomial =
+    fun a b c z : Z => binomial a b z * z + c
+        : Z -> Z -> Z -> Z -> Z
+*)
+
+
+Section h_def.
+    Variables a b : Z.
+    Let s : Z := a + b.
+    Let d : Z := a - b.
+    Definition h : Z := s*s + d*d.
+End h_def.
+
+(* when section is closed local definitions are replaced
+   with local let-in bindings
+*)
+Print h.
+(*
+    h =
+    fun a b : Z => let s := a + b in let d := a - b in s * s + d * d
+        : Z -> Z -> Z
+*)
+
+
+(******************************************************************)
+(** 2.4 Computing **)
